8ed31f7f7c6b6aba39a57f1a4634f11b6788ca84
Eder Lima <devilsouls666@gmail.com>
2022-05-01 13:04:21 -0300

    docs: add docs for the date utils

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,3 +1,16 @@
+/**
+ * Function for parsing a date to a string formatted as 'DAY MONTH YEAR' in
+ * the 'en-GB' locale.
+ *
+ * ## Example
+ *
+ * ```tsx
+ * parseDate('2019-01-01') // '01 January 2019'
+ * ```
+ *
+ * @param date Date to be parsed.
+ * @returns A parsed version of the date.
+ */
 export function parseDate(date: string): string {
   return new Intl.DateTimeFormat('en-GB', {
     day: '2-digit',
