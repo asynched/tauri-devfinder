05d30640dc68df1ce0cd7b07797a5ed931bdd5a7
Eder Lima <devilsouls666@gmail.com>
2022-05-01 13:04:39 -0300

    docs: add docs for the ui event utils

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,5 +1,19 @@
 import React from 'react'
 
+/**
+ * This is a helper function for handling events inside a component, it's used
+ * as a higher-order function to wrap a callback function.
+ *
+ * ## Example
+ *
+ * ```tsx
+ * <form onSubmit={preventDefault(() => void 0)} />
+ * ```
+ *
+ * @param handler Handler function to be executed after the event is prevented
+ * from it's default behavior.
+ * @returns A closure that can be used to attach the event listener.
+ */
 export function preventDefault<F extends () => any>(handler: F) {
   return (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
