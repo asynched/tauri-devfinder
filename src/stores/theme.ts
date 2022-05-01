bddc74d0d46354723f6793b8d1a6ffff727c5cbf
Eder Lima <devilsouls666@gmail.com>
2022-05-01 13:04:03 -0300

    docs: add docs for the ui theme functions

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -4,6 +4,15 @@ import { getTheme, toggleTheme as domToggleTheme } from '@/utils/theme'
 
 const themeBox = createBox(getTheme())
 
+/**
+ * Toggles the application theme from dark to light and vice versa.
+ *
+ * ## Example
+ *
+ * ```tsx
+ * toggleTheme() // void
+ * ```
+ */
 export function toggleTheme() {
   themeBox.set(() => {
     domToggleTheme()
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,7 +1,19 @@
+/**
+ * Theme is an enum of the available themes, those being 'light' and 'dark'.
+ */
 type Theme = 'light' | 'dark'
 
 const kStorage = 'dev_finder:theme'
 
+/**
+ * Initializer for the theme configurations, must be executed on app startup.
+ *
+ * ## Example
+ *
+ * ```tsx
+ * useEffect(themeSetup, [])
+ * ```
+ */
 export function themeSetup() {
   const colorPreference = localStorage.getItem(kStorage) as Theme
 
@@ -10,6 +22,8 @@ export function themeSetup() {
     return
   }
 
+  // If the key doesn't exist in local storage, initialize the application
+  // with the user default preference in the OS.
   const preference = window.matchMedia('(prefers-color-scheme: dark)').matches
     ? 'dark'
     : 'light'
@@ -17,18 +31,56 @@ export function themeSetup() {
   localStorage.setItem(kStorage, preference)
 }
 
+/**
+ * Helper function for getting the current application theme.
+ *
+ * ## Example
+ *
+ * ```tsx
+ * getTheme() // 'light' | 'dark'
+ * ```
+ *
+ * @returns The current application theme.
+ */
 export function getTheme(): Theme {
   const theme = localStorage.getItem(kStorage) as Theme
+
+  // Defaults the theme to light if the key
+  // doesn't exist in the local storage.
   return theme || 'light'
 }
 
+/**
+ * Helper function for setting the application theme.
+ *
+ * ## Example
+ *
+ * ```tsx
+ * setTheme('dark') // void
+ * ```
+ *
+ * @param theme Sets the application theme.
+ */
 export function setTheme(theme: Theme) {
+  // Clears the document class list and adds the theme
+  // passed in as a parameter. This is mainly due to
+  // the document.classList.add() method concatenating
+  // both 'light' and 'dark' classes.
   document.documentElement.classList.remove('dark', 'light')
   document.documentElement.classList.add(theme)
 
   localStorage.setItem(kStorage, theme)
 }
 
+/**
+ * Toggles the application theme from dark to light and vice versa.
+ *
+ * ## Example
+ *
+ * ```tsx
+ * toggleTheme() // void
+ * ```
+ */
 export function toggleTheme() {
   const current = localStorage.getItem(kStorage)
 
